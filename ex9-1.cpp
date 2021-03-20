da70bca33901aee7fd38d27fafe9c88ae036cabd
Toby Law <toby@tzcl.me>
2021-03-20 12:38:23 +1100

    Read through ex9.1

---
+++ ex9-1.cpp
@@ -0,0 +1,21 @@
+// Design a stack that includes a max operation, in addition to push and pop.
+// the max method should return the maximum value stored in the stack.
+//
+// Hint: use additional storage to track the maximum value.
+
+#include <bits/stdc++.h>
+
+// What does design a stack mean? Implement a stack?
+// Doing these problems is annoying without boilerplate code.
+//
+// What's interesting about this question is that you can pass your own structs
+// to std::stack
+
+class Stack {
+public:
+  Stack() {}
+
+private:
+};
+
+int main() {}
